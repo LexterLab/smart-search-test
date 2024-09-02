70a37205706fe3a3dc9013bbee4fb00db689d04a
moldovexc <alexanderparpulansky@gmail.com>
2024-09-02 21:41:06 +0300

    Add validations

--- src/main/java/com/test/smartsearch/exception/GlobalExceptionHandler.java
+++ src/main/java/com/test/smartsearch/exception/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package com.test.smartsearch.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,5 +23,16 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorDetails> handleDefaultException(Exception exception){
         ErrorDetails errorDetails = new ErrorDetails(exception.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+        Map<String, String> errorMessages = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName =  ((FieldError)error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errorMessages.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/test/smartsearch/payload/updatemovie/UpdateMovieRequest.java
+++ src/main/java/com/test/smartsearch/payload/updatemovie/UpdateMovieRequest.java
@@ -2,14 +2,20 @@ package com.test.smartsearch.payload.updatemovie;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
+import org.hibernate.validator.constraints.Length;
 
 @Builder
 public record UpdateMovieRequest(
         @JsonIgnore
         String movieId,
         @Schema(example = "Harry Potter")
+        @NotBlank(message = "Field name cannot be blank")
+        @Length(max = 100, message = "Field name must be max 100 chars")
         String name,
         @Schema(example = "Romance")
+        @NotBlank(message = "Field genre cannot be null")
+        @Length(min = 3, max = 100, message = "Field genre must be 3-100 chars")
         String genre
 ) {}
